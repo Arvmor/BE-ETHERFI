04fed555148d562523d29741dcb9188434534a8f
Arvin Morawej <arvin.morawej@gmail.com>
2024-06-06 21:33:55 -0400

    Added logger

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 pub use std::io::Result;
-pub use actix_web::{HttpServer, App, get, post, web, HttpResponse, Responder};
+pub use actix_web::{HttpServer, App, get, post, web, HttpResponse, Responder, middleware};
 pub use actix_cors::Cors;
+pub use tracing::level_filters::LevelFilter;
+pub use tracing_subscriber::{fmt::layer, Registry, layer::SubscriberExt, Layer, util::SubscriberInitExt};
--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,25 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> Result<()> {
+
+    // Terminal Logger
+    let terminal_layer = layer()
+        .with_thread_ids(false)
+        .with_thread_names(false)
+        .with_target(false)
+        .without_time()
+        .with_line_number(false)
+        .with_file(false)
+        .with_filter(LevelFilter::INFO);
+
+    Registry::default()
+        .with(terminal_layer)
+        .init();
     
     HttpServer::new(move || {
         App::new()
             .wrap(Cors::permissive())
+            .wrap(middleware::Logger::default())
             .service(index)
     })
     .bind("0.0.0.0:1337")?
